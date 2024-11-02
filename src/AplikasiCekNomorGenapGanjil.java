b15d1cfc258e720e7dfc46a2641c82af0c64a812
Hilmanaja <muhammadhilman1806@gmail.com>
2024-11-02 12:08:18 +0800

    Menambahkan GUI components dan mendesain aplikasi

--- src/AplikasiCekNomorGenapGanjil.java
+++ src/AplikasiCekNomorGenapGanjil.java
@@ -8,12 +8,12 @@
  *
  * @author Hilman
  */
-public class CekNomorGenapGanjil extends javax.swing.JFrame {
+public class AplikasiCekNomorGenapGanjil extends javax.swing.JFrame {
 
     /**
-     * Creates new form CekNomorGenapGanjil
+     * Creates new form AplikasiCekNomorGenapGanjil
      */
-    public CekNomorGenapGanjil() {
+    public AplikasiCekNomorGenapGanjil() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class CekNomorGenapGanjil extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AplikasiCekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AplikasiCekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AplikasiCekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AplikasiCekNomorGenapGanjil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CekNomorGenapGanjil().setVisible(true);
+                new AplikasiCekNomorGenapGanjil().setVisible(true);
             }
         });
     }
